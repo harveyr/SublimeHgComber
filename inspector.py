9802e3133e04959155fc38bdf181aba2fcfe5283
Harvey Rogers <harveyr@gmail.com>
2013-07-25 05:56:40 -0700

    Basic grep and log are working

--- inspector.py
+++ inspector.py
@@ -32,14 +32,14 @@ class CommandRunner(threading.Thread):
 
 
 class HgChangeset(object):
-    def __init__(self, log_str):
-        tokens = log_str.split('|')
-        self.revision = tokens[0]
-        self.node = tokens[1]
-        self.branch = tokens[2]
-        self.author = tokens[3]
-        self.date = tokens[4]
-        self.description = tokens[5]
+    def __init__(self, revision, node, branch, author, date, age, description):
+        self.revision = revision
+        self.node = node
+        self.branch = branch
+        self.author = author
+        self.date = date
+        self.age = age
+        self.description = description
 
     def __str__(self):
         desc = self.description
@@ -84,46 +84,72 @@ class HgLogCombGrepFileCommand(sublime_plugin.TextCommand):
             self.search_str, self.file_name, output)
         self.update_panel(output)
 
+    def update_panel(self, text, append=False):
+        self.view.run_command(
+            'hg_log_inspect_panel',
+            {
+                'text': text,
+                'append': append
+            }
+        )
 
-class HgLogCombGrepFileCommand(sublime_plugin.TextCommand):
+
+class HgLogCombLogFileCommand(sublime_plugin.TextCommand):
 
     MAX_CHANGESETS = 30
 
+    @property
+    def current_changeset(self):
+        return self.changesets[self.current_changeset_index]
+
     def run(self, edit):
+        self.changesets = []
+        self.current_changeset_index = None
+
         self.file_name = self.view.file_name()
         if not self.file_name:
             print('Not in a saved file. Goodbye.')
             return
+        self.window = sublime.active_window()
         self.file_wd = os.path.split(self.file_name)[0]
+        self.get_log()
+        self.show_menu()
 
-    def get_changesets(self):
-        cmd = 'hg log --limit {} --style compact {}'.format(
-            self.MAX_CHANGESETS,
-            self.file_name)
-        # cmd += "--template '{rev}|{node|short}|{branch}|{author|person}|{date|isodate}|{desc}\\n' "
-        # cmd += self.file_name
+    def get_log(self):
+        # cmd = 'hg log --limit {} --style compact {}'.format(
+        #     self.MAX_CHANGESETS,
+        #     self.file_name)
+        log_template = '{rev}|{node|short}|{branch}|{author|person}|{date|isodate}|{date|age}|{desc}||'
+        cmd = 'hg log --limit {limit} --template {template} {file}'.format(
+            limit=self.MAX_CHANGESETS,
+            template=log_template,
+            file=self.file_name)
         output = self.run_command(cmd)
-        print('output: {v}'.format(v=output))
-        # self.parse_hg_log(output)
+        changesets = [c for c in output.split('||') if c]
+        self.parse_changesets(changesets)
 
-    def get_changesets_old(self):
-        cmd = 'hg log --limit {} '.format(self.MAX_CHANGESETS)
-        cmd += "--template '{rev}|{node|short}|{branch}|{author|person}|{date|isodate}|{desc}\\n' "
-        cmd += self.file_name
-        output = self.run_command(cmd)
-        self.parse_hg_log(output)
-
-    def parse_hg_log(self, hg_log_output):
+    def parse_changesets(self, raw_changesets):
         self.changesets = []
-        for l in [l for l in hg_log_output.splitlines() if l]:
-            self.changesets.append(HgChangeset(l))
+        for c in raw_changesets:
+            print('c: {v}'.format(v=c))
+            tokens = c.split('|')
+            self.changesets.append(HgChangeset(
+                revision=tokens[0],
+                node=tokens[1],
+                branch=tokens[2],
+                author=tokens[3],
+                date=tokens[4],
+                age=tokens[5],
+                description=tokens[6]
+            ))
 
     def show_menu(self):
-        sublime.ok_cancel_dialog('Search next changeset?')
-        # self.menu_items = []
-        # for c in self.changesets:
-        #     self.menu_items.append(str(c))
-        # self.view.show_popup_menu(self.menu_items, self.menu_on_done)
+        self.menu_items = []
+        for c in self.changesets:
+            menu_item_str = '{} {}: {}'.format(
+                c.author, c.age, c.description)
+            self.menu_items.append(menu_item_str)
+        self.window.show_quick_panel(self.menu_items, self.menu_on_done)
 
     def menu_on_done(self, index):
         self.current_changeset_index = index
@@ -137,13 +163,14 @@ class HgLogCombGrepFileCommand(sublime_plugin.TextCommand):
             on_change=self.on_prompt_change,
             on_cancel=self.cancel)
 
-    def search_current_changeset(self, text):
+    def search_current_changeset(self):
+        changeset = self.current_changeset
         cmd = 'hg log -p -r {rev}:{node}'.format(
-            rev=self.current_changeset.revision,
-            node=self.current_changeset.node)
+            rev=changeset.revision,
+            node=changeset.node)
 
         patch = self.run_command(cmd)
-        self.update_panel('{}\n{}'.format(self.current_changeset, patch))
+        self.update_panel('{}\n{}'.format(changeset, patch))
 
         self.current_changeset_index += 1
         # self.prompt_next_changeset()
@@ -173,7 +200,8 @@ class HgLogCombGrepFileCommand(sublime_plugin.TextCommand):
             'hg_log_inspect_panel',
             {
                 'text': text,
-                'append': append
+                'append': append,
+                'diff_syntax': True
             }
         )
 
@@ -191,15 +219,15 @@ class HgLogCombShowPanelCommand(sublime_plugin.TextCommand):
 class HgLogInspectPanelCommand(sublime_plugin.TextCommand):
     panel = None
 
-    def run(self, edit, text, append=False):
+    def run(self, edit, text, append=False, diff_syntax=False):
         window = sublime.active_window()
-        # if not self.panel:
-        self.panel = window.create_output_panel(PANEL_NAME)
+        panel = window.create_output_panel(PANEL_NAME)
         start_point = 0
         if append:
-            start_point = self.panel.size()
-        self.panel.insert(edit, start_point, text)
-        # self.panel.set_syntax_file("Packages/Diff/Diff.tmLanguage")
+            start_point = panel.size()
+        panel.insert(edit, start_point, text)
+        if diff_syntax:
+            panel.set_syntax_file("Packages/Diff/Diff.tmLanguage")
         window.run_command(
             'show_panel',
             {'panel': 'output.{}'.format(PANEL_NAME)}
